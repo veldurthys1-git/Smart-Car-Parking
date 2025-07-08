70d60d453cbc1f781b521e4fb05a30ea269f4d83
Charan <veldurthysaicharan5@gmail.com>
2025-07-08 13:47:19 -0700

    Added deploy script and GitHub Pages base config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Smart-Car-Parking/', // Name of your GitHub repo
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+})
